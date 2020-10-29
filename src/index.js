23a695a2e1dc99b406b63d3c7e89aa6e6dbc72ef
Charlie Davison <charlie.davison@lifeworks.com>
2020-10-29 10:24:00 +0000

    Working POC for updating datahooks

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
-const getDisplayName = require('./get-display-name');
+const getDisplayName = require('./getDisplayName.js');
 
-export default function (babel) {
+module.exports = function (babel) {
     const { types: t } = babel;
 
     return {
         visitor: {
             Program: function Program(path, state) {
-                const property = state.opts.property || 'data-hook';
+                const property = state.opts.property;
 
                 let displayName = getDisplayName(t)(path, state);
 
                 path.traverse({
                     JSXElement: function JSXElement(path2) {
                         const attributes = path2.node.openingElement.attributes;
-                        const existingDataHookIndex = Array.from(attributes).findIndex(attribute => attribute.name.name === property)
+                        const attributesArray = Array.from(attributes);
+                        let isJSX = true;
+
+                        const existingDataHookIndex = attributesArray.findIndex(attribute => {
+                            if (attribute.name && attribute.name.name) {
+                                if(attribute.name.name === property) return true;
+
+                                if (attribute.name.name === 'data-hook' && typeof attribute.value.value === 'string') {
+                                    isJSX = false;
+                                    return true;
+                                }
+                            }
+                            return false;
+                        });
 
                         if (existingDataHookIndex === -1) return;
 
-                        const hookValue = Array.from(attributes)[existingDataHookIndex].value.value;
+                        const hookValue = attributesArray[existingDataHookIndex].value.value;
                         displayName += `-${hookValue}`;
 
-                        path2.node.openingElement.attributes.splice(existingDataHookIndex, 1)
+                        path2.node.openingElement.attributes.splice(existingDataHookIndex, 1);
 
                         path2.node.openingElement.attributes.push(
-                            t.jSXAttribute(t.jSXIdentifier(property), t.stringLiteral(`${displayName}`)),
+                            t.jSXAttribute(t.jSXIdentifier(isJSX ? property : 'data-hook'), t.stringLiteral(`${displayName}`)),
                         );
                     },
                 });
